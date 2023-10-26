d5d50328373fc8f30d3e0e94a55e25258db2d2ca
Julia Ajpert <jajpert@gmail.com>
2023-10-26 10:51:44 -0400

    exercicio de deploy

--- Unidade 4/back-integral-query-builder/classe/src/index.js
+++ Unidade 4/back-integral-query-builder/classe/src/index.js
@@ -4,8 +4,12 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(rotas);
 
-app.listen(3000);
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('Servidor na porta', port);
+});
